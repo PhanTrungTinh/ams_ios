66e214f4d845e0e6798136e83177eb95028afe84
217prod <217prod@gmail.com>
2018-06-08 21:30:14 +0700

    asm02

---
+++ asm02_Autolayout/asm02_AutolayoutTests/asm02_AutolayoutTests.swift
@@ -0,0 +1,36 @@
+//
+//  asm02_AutolayoutTests.swift
+//  asm02_AutolayoutTests
+//
+//  Created by TinhPhan on 6/8/18.
+//  Copyright © 2018 TinhPhan. All rights reserved.
+//
+
+import XCTest
+@testable import asm02_Autolayout
+
+class asm02_AutolayoutTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
